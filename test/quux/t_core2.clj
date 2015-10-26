de70faf95c23544de182d4fd519c217cb520e454
Daniel Lebrero <dlebrero@gmail.com>
2015-10-26 23:38:36 +0100

    step three. Deleting the new test namespace

--- test/quux/t_core2.clj
+++
@@ -1,9 +0,0 @@
-(ns quux.t-core2
-  (:use midje.sweet)
-  (:use [quux.core]))
-
-(fact "default value is returned for empty sequences"
-      (first-element [] :default1) => :default
-      (first-element '() :default1) => :default
-      (first-element nil :default1) => :default
-      (first-element (filter even? [1 3 5]) :default) => :default)
